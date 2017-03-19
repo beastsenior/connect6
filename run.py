71200f7027ec0d447caf9d580888cf6b657278be
beast senior <beastsenior.qsh@gmail.com>
2017-03-19 05:30:50 -0400

    1

---
+++ run.py
@@ -0,0 +1,13 @@
+from connect6 import Cconnect6
+
+app=Cconnect6()
+
+#m=[[0,0,1,1],[1,0,0,1],[0,0,0,0],[-1,-1,-1,-1]]
+#app.redraw(m)
+
+#app.canvas.itemconfig(app.oval, state='hidden')
+#app.canvas.move(app.oval, 10, 10)
+
+
+
+app.mainloop()
